8569dc00cb71a798ad5bfc17876e282ee0f3b534
Gurvan Dumarchat <gurvandumarchat@MacBook-Air-de-Gurvan.local>
2023-12-08 05:51:42 +0100

    Grid implanted

--- src/Components/Game/Cellule.jsx
+++ src/Components/Game/Cellule.jsx
@@ -1,16 +1,36 @@
 import Ph from '../../assets/ph1.svg';
 import Ph2 from '../../assets/ph2.svg';
+import Ph3 from '../../assets/ph3.svg';
+import Ph4 from '../../assets/ph4.svg';
 export default function Cellule(props){
-    this.row = props.row;
-    this.col = props.col;
+    let row = props.row;
+    let col = props.col;
     const infrastructures = [
         Ph,
         Ph2,
         "",
     ]
+
+    const handleNature = () =>{
+        switch (props.contenu){
+            case 'M':
+                return Ph;
+                break;
+            case 'B':
+                return Ph2;
+                break;
+            case 'D':
+                return Ph3;
+                break;
+            default:
+                return Ph4;
+                break;
+        }
+    }
+
     return(
         <div className="cell">
-            <p>{props.nature}</p>
+            <img src={handleNature()} alt="" width='50px' height='50px'/>
         </div>
     )
 }
--- src/Components/Game/Plateau.jsx
+++ src/Components/Game/Plateau.jsx
@@ -1,25 +1,30 @@
 import '../Styles/plateau.css'
-import {useEffect, useState,} from "react";
+import {useEffect, useState} from "react";
 import Axios from "axios";
 import Cellule from "./Cellule";
 
-
 export default function Plateau() {
-    const [elements, setElements] = useState([]);
-    const createRow = (elements) => {
+    const [elements, setElements] = useState([
+        ['M']
+    ]);
 
-    }
     useEffect(() => {
         Axios.get("http://91.92.252.31:5000/plateau_golmon")
             .then(res => {
-                setElements(res.data.mat)
-                console.log(elements)
+                setElements(res.data.mat);
             })
     }, []);
 
     return (
         <div className="plateau">
-            {createRow(elements)}
+            {
+                elements.map((elem,index) => (
+                    <div key={index} className={'row'}>
+                        {elements[index].map((eleme,i) => <Cellule key={i} row={index} col={i} contenu={eleme}/>)}
+                    </div>
+                    )
+                )
+            }
         </div>
-    )
+    );
 }
